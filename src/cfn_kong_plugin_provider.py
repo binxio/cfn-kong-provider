06d6035968719c907d0993e026469edfff776f6a
lionel <fuliang.guo@moodys.com>
2019-10-31 18:20:47 +0800

    upgrade Custom::KongPlugin to version 1.1

--- src/cfn_kong_plugin_provider.py
+++ src/cfn_kong_plugin_provider.py
@@ -29,17 +29,32 @@ request_schema = {
                 "name": {
                     "type": "string"
                 },
-                "consumer_id": {
-                    "type": "string"
+                "consumer": {
+                    "type": "object",
+                    "required": ["id"],
+                    "properties": {
+                        "id": {
+                            "type": "string"
+                        }
+                    }
                 },
-                "service_id": {
-                    "type": "string"
+                "service": {
+                    "type": "object",
+                    "required": ["id"],
+                    "properties": {
+                        "id": {
+                            "type": "string"
+                        }
+                    }
                 },
-                "route_id": {
-                    "type": "string"
-                },
-                "api_id": {
-                    "type": "string"
+                "route": {
+                    "type": "object",
+                    "required": ["id"],
+                    "properties": {
+                        "id": {
+                            "type": "string"
+                        }
+                    }
                 },
                 "config": {
                     "type": "object"
@@ -55,37 +70,6 @@ class KongPluginProvider(KongProvider):
     def __init__(self):
         super(KongPluginProvider, self).__init__('plugins', 'Plugin')
         self.request_schema = request_schema
-
-    def create(self):
-        if 'api_id' in self.get('Plugin'):
-            self.create_on_api()
-        else:
-            super(KongPluginProvider, self).create()
-
-    def create_on_api(self):
-        """
-        we noticed that creating a plugin on an API through the /plugins interface
-        did not activate the http-log plugin. So, we create it on the /apis/<api>/plugins
-        interface instead.
-        """
-        if self.add_jwt():
-            try:
-                url = '%s/apis/%s/plugins' % (self.get('AdminURL'), self.get('Plugin').get('api_id'))
-                properties = self.get('Plugin').copy()
-                del properties['api_id']
-                response = requests.post(url, headers=self.headers, json=properties)
-                if response.status_code in (200, 201):
-                    r = response.json()
-                    self.physical_resource_id = r['id']
-                    self.set_attribute('id', self.physical_resource_id)
-                else:
-                    self.physical_resource_id = 'failed-to-create'
-                    self.fail('Could not create the %s, %s' % (self.property_name, response.text))
-            except IOError as e:
-                self.physical_resource_id = 'failed-to-create'
-                self.fail('Could not create the %s, %s' % (self.property_name, str(e)))
-        else:
-            self.physical_resource_id = 'failed-to-create'
 
 provider = KongPluginProvider()
 
